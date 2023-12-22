73897a7155142a8ff8547261fe96bda041beda88
Artyomenko Vladislav <81217105+a2vQA@users.noreply.github.com>
2023-12-22 22:33:47 +0300

    New repo with simple test

---
+++ src/test/java/guru/qa/SimpleJUnitTest.java
@@ -0,0 +1,43 @@
+package guru.qa;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SimpleJUnitTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("\n\n### @BeforeAll\n");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("### @AfterAll\n\n");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("###     @BeforeEach");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("###     @AfterEach\n");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("###         @Test firstTest");
+        Assertions.assertTrue( 2 * 2 == 4);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("###       @Test secondTest");
+        Assertions.assertTrue( 2 + 2 == 4);
+    }
+}
