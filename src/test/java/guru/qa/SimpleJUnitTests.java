0e92e3cb80384f504ddce969f6905b608c6ebcc2
Artyomenko Vladislav <81217105+a2vQA@users.noreply.github.com>
2024-01-08 06:20:43 +0300

    HW #5 Added WikiSelenideTest

--- src/test/java/guru/qa/SimpleJUnitTests.java
+++ src/test/java/guru/qa/SimpleJUnitTests.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class SimpleJUnitTest extends BaseTest {
+public class SimpleJUnitTests extends BaseTest {
 
     @BeforeAll
     static void beforeAll() {
---
+++ src/test/java/guru/qa/WikiSelenideTests.java
@@ -0,0 +1,58 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class WikiSelenideTests {
+
+    private final String JUNIT_FIRST_CODE = """
+        @ExtendWith({SoftAssertsExtension.class})
+        class Tests {
+          @Test
+          void test() {
+            Configuration.assertionMode = SOFT;
+            open("page.html");
+
+            $("#first").should(visible).click();
+            $("#second").should(visible).click();
+          }
+        }""";
+    private final String JUNIT_SECOND_CODE = """
+        class Tests {
+          @RegisterExtension
+          static SoftAssertsExtension softAsserts = new SoftAssertsExtension();
+
+          @Test
+          void test() {
+            Configuration.assertionMode = SOFT;
+            open("page.html");
+
+            $("#first").should(visible).click();
+            $("#second").should(visible).click();
+          }
+        }""";
+
+    @Test
+    public void wikiSelenideTest() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab")
+                .click();
+        $("#wiki-body .markdown-body ul")
+                .shouldHave(text("Soft assertions"))
+                .$(byText("Soft assertions"))
+                .click();
+
+        $("#user-content-3-using-junit5-extend-test-class")
+                .ancestor("h4")
+                .sibling(0)
+                .shouldHave(text(JUNIT_FIRST_CODE));
+        $("#user-content-3-using-junit5-extend-test-class")
+                .ancestor("h4")
+                .sibling(2)
+                .shouldHave(text(JUNIT_SECOND_CODE));
+    }
+}
