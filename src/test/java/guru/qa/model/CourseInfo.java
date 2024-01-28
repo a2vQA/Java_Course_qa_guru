98a2dba42524a73adac48648e67af2c87284f0f0
Artyomenko Vladislav <81217105+a2vQA@users.noreply.github.com>
2024-01-28 19:10:18 +0300

    HW #11

---
+++ src/test/java/guru/qa/model/CourseInfo.java
@@ -0,0 +1,22 @@
+package guru.qa.model;
+
+public class CourseInfo {
+    private String programmingLanguage;
+    private int groupNumber;
+
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+
+    public void setProgrammingLanguage(String programmingLanguage) {
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    public int getGroupNumber() {
+        return groupNumber;
+    }
+
+    public void setGroupNumber(int groupNumber) {
+        this.groupNumber = groupNumber;
+    }
+}
