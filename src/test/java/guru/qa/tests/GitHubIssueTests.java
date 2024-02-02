2cf6c57d884da8a058e7bfd8daf85bb3d424e2c0
Artyomenko Vladislav <81217105+a2vQA@users.noreply.github.com>
2024-02-02 03:25:46 +0300

    HW #12

--- src/test/java/guru/qa/tests/GitHubIssueTests.java
+++ src/test/java/guru/qa/tests/GitHubIssueTests.java
@@ -3,7 +3,11 @@ package guru.qa.tests;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import guru.qa.pages.GitHubSteps;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.withText;
@@ -12,11 +16,16 @@ import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 import static org.openqa.selenium.By.linkText;
 
+@Feature("Issue в репозитории")
+@Story("Поиск issue в репозитории")
 public class GitHubIssueTests {
 
     private final String REPOSITORY = "eroshenkoam/allure-example";
     private final Integer ISSUE_NUMBER = 87;
 
+
+    @Owner("vvartemenko")
+    @DisplayName("Поиск issue в репозитории с Selenide listener")
     @Test
     public void testNativeListenerStep() {
         SelenideLogger.addListener("allure", new AllureSelenide());
@@ -29,6 +38,8 @@ public class GitHubIssueTests {
         $(withText("#" + ISSUE_NUMBER)).shouldBe(Condition.visible);
     }
 
+    @Owner("vvartemenko")
+    @DisplayName("Поиск issue в репозитории с Selenide lambda steps")
     @Test
     public void testLambdaStep() {
         SelenideLogger.addListener("allure", new AllureSelenide());
@@ -50,7 +61,8 @@ public class GitHubIssueTests {
         });
     }
 
-
+    @Owner("vvartemenko")
+    @DisplayName("Поиск issue в репозитории с Selenide annotations")
     @Test
     public void testAnnotatedSteps() {
         SelenideLogger.addListener("allure", new AllureSelenide());
