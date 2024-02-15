c43297b44b96cc6e9474277e37b3f9f985c4f7bd
Artyomenko Vladislav <81217105+a2vQA@users.noreply.github.com>
2024-02-15 11:23:13 +0300

    HW15: Parameterized Jenkins (#2)
    * HW 15 finished
    
    * Fix try
    
    * Fix  try
    
    * Fix try
    
    * Fix try
    
    * Fix try
    
    * Fix try
    
    * Try fix
    
    * Try fix
    
    * Try fix
    
    * Try fix
    
    * Final HW15
    
    * Final HW15
    
    * Trying more features of Owner
    
    * Update BaseTest
    
    * Rollback to first working version

---
+++ src/test/java/guru/qa/config/DriverConfig.java
@@ -0,0 +1,25 @@
+package guru.qa.config;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "classpath:config/driver.properties"
+})
+public interface DriverConfig extends Config {
+    @Key("browser.name")
+    @DefaultValue("chrome")
+    String browserName();
+
+    @Key("browser.version")
+    @DefaultValue("100.0")
+    String browserVersion();
+
+    @Key("browser.size")
+    @DefaultValue("1920x1080")
+    String browserSize();
+
+    @Key("browser.remote.url")
+    @DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub")
+    String browserRemoteUrl();
+}
--- src/test/java/guru/qa/tests/BaseTest.java
+++ src/test/java/guru/qa/tests/BaseTest.java
@@ -3,23 +3,29 @@ package guru.qa.tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import guru.qa.config.DriverConfig;
 import guru.qa.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
+import java.util.Objects;
 
 public class BaseTest {
 
     @BeforeAll
     static void browserSettings() {
+        DriverConfig driverConfig = ConfigFactory.create(DriverConfig.class);
+
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "100";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browser = System.getProperty("browser", driverConfig.browserName());
+        Configuration.browserVersion = System.getProperty("browserVersion", driverConfig.browserVersion());
+        Configuration.browserSize = System.getProperty("browserSize", driverConfig.browserSize());
+        Configuration.remote = System.getProperty("browserRemoteUrl", driverConfig.browserRemoteUrl());
 
         SelenideLogger.addListener("allure", new AllureSelenide());
 
@@ -36,7 +42,9 @@ public class BaseTest {
     void addAttachments() {
         Attach.screenshotAs("Last Screenshot");
         Attach.pageSource();
-        Attach.browserConsoleLogs();
+        if (!Objects.equals(Configuration.browser, "firefox")) {
+            Attach.browserConsoleLogs();
+        }
         Attach.addVideo();
         Selenide.closeWebDriver();
     }
